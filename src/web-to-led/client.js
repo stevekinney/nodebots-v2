de3fd037f01350473a43f6fe28af9aaa62944764
Steve Kinney <hello@stevekinney.net>
2023-08-13 05:45:10 -0600

    Add back in Web to LED

---
+++ src/web-to-led/client.js
@@ -0,0 +1,7 @@
+import post from '#utilities/post';
+
+const lightSwitch = document.querySelector('#lightswitch');
+
+lightSwitch.addEventListener('click', async () => {
+  await post('/api/light');
+});
