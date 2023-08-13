8ca7428b2bb9b7ac351cd766b6037e3ab79a38f1
Steve Kinney <hello@stevekinney.net>
2023-08-13 05:43:39 -0600

    Add base styles

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ import rootPath from 'app-root-path';
 
 export default {
   plugins: {
+    'postcss-import': {},
     tailwindcss: {
       config: rootPath.resolve('./tailwind.config.js'),
     },
